e1f0bab1ec5e427b9b380385530bac147e79424b
Maryam308 <mariam601me@gmail.com>
2026-02-22 19:50:40 +0300

    feat: initial project setup - Spring Boot, Docker, PostgreSQL connected

---
+++ backend/src/main/java/com/pathwise/backend/PathWiseApplication.java
@@ -0,0 +1,13 @@
+package com.pathwise.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathWiseApplication.class, args);
+	}
+
+}
